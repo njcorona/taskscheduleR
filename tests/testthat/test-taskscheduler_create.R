b27af5c45cfa4683a01045d2b730c02e4b550b73
Jan Wijffels <jwijffels@bnosac.be>
2017-03-03 10:34:20 +0100

    prepare for cran

---
+++ tests/testthat/test-taskscheduler_create.R
@@ -0,0 +1,17 @@
+context("taskscheduler-examples")
+
+test_that("taskscheduleR examples can be scheduled as expected", {
+  skip_on_cran()
+  library(taskscheduleR)
+  myscript <- system.file("extdata", "helloworld.R", package = "taskscheduleR")
+  
+  ## run script once within 62 seconds
+  expect_warning(taskscheduler_create(taskname = "myfancyscript", rscript = myscript, 
+                       schedule = "ONCE", starttime = format(Sys.time() + 62, "%H:%M")), NA)
+  
+  ## get a data.frame of all tasks
+  expect_warning(tasks <- taskscheduler_ls(), NA)
+
+  ## delete the tasks
+  expect_warning(taskscheduler_delete(taskname = "myfancyscript"), NA)
+})
